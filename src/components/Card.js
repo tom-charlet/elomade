b4b098d7c659af4da014af6150c568b1744fd4b8
Tom CHARLET <charlettom11@gmail.com>
2025-05-22 23:40:45 +0200

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,9 @@
 
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
-import { motion } from "framer-motion"
 import { useRouter } from 'next/navigation'
 
 const Heading = dynamic(() => import('./Heading'))
-
-const cardVariants = {
-    visible: { x: 0, opacity: 1 },
-    hidden: { x: 100, opacity: 0 }
-}
 
 const Card = ({ title, images, categories, slug, grid }) => {
     const router = useRouter()
@@ -19,11 +13,7 @@ const Card = ({ title, images, categories, slug, grid }) => {
         if (slug) router.push(`/creations/${slug}`)
     }
 
-    return <motion.div
-        className={`h-full flex flex-col gap-4 ${grid ? "w-full" : "w-[320px] lg:w-[385px]"}`}
-        animate={cardVariants}
-        variants={cardVariants}
-    >
+    return <div className={`h-full flex flex-col gap-4 ${grid ? "w-full" : "w-[320px] lg:w-[385px]"}`}>
         <button onClick={handleClick} className='cursor-pointer aspect-[14/16] lg:aspect-[13/16] bg-beige-300 rounded-2xl overflow-hidden'>
             <Image width={0} height={0} sizes='100vw' alt={images?.[0]?.alt ?? title ?? ""} src={images?.[0]?.url ?? "/placeholder.svg"} className='object-cover w-full h-full' />
         </button>
@@ -33,7 +23,7 @@ const Card = ({ title, images, categories, slug, grid }) => {
                 return item ? `${index > 0 ? ` - ${item.title}` : item.title}` : null
             })}</Heading>}
         </div>
-    </motion.div>
+    </div>
 }
 
 export default Card
