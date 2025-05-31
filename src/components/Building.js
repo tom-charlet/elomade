383334874bb3e63e45a239d4f12dcb6e3007fd80
Tom CHARLET <charlettom11@gmail.com>
2025-05-31 19:04:56 +0200

    update Socials + responsive

--- src/components/Building.js
+++ src/components/Building.js
@@ -5,14 +5,14 @@ const Button = dynamic(() => import('./Button'))
 const Animation = dynamic(() => import('../components/Animation'))
 
 const Building = () => {
-    return <section className='bg-beige-100 min-h-[calc(100svh-80px)] flex items-center justify-center px-6 pt-32 pb-16 md:pt-[120px] md:pb-20'>
-        <Animation tag="section" type={["fade", "move"]} direction='up' stagger={0.1} value={{ move: [50, 0] }} transition={{ duration: 1.5 }} className='flex justify-center flex-col gap-6 max-w-[640px]'>
-            <Heading level="2">Site en construction</Heading>
-            <Heading level="3">Le site <strong className='text-red-700'>elomade.fr</strong> est actuellement en cours de création. <br className='hidden md:block' />Revenez très prochainement pour découvrir des pièces en crochet uniques !</Heading>
-            <div className='flex flex-col md:flex-row md:items-center gap-4 md:gap-3 mt-2'>
-                <div className='flex items-center gap-3 w-full'>
-                    <Button href="https://www.tiktok.com/@elomade.it" target="_blank" icon="tiktok" variante="stroke-red" className="basis-full">elomade.it</Button>
-                    <Button href="https://www.instagram.com/elomadeit" target="_blank" icon="instagram" variante="stroke-red" className="basis-full">elomadeit</Button>
+    return <section className='bg-beige-100 min-h-svh md:min-h-[calc(100svh-80px)] flex items-center justify-center px-6 pt-32 pb-16 md:pt-[120px] md:pb-20 text-center lg:text-left'>
+        <Animation tag="section" type={["fade", "move"]} direction='up' stagger={0.1} value={{ move: [50, 0] }} transition={{ duration: 1.5 }} className='flex justify-center flex-col gap-6 max-w-[680px]'>
+            <Heading level="1">Site en construction</Heading>
+            <Heading level="3" className="text-balance">Le site <strong className='text-red-700'>elomade.fr</strong> est actuellement en cours de création. <br className='hidden md:block' />Revenez très prochainement pour découvrir des pièces en crochet uniques !</Heading>
+            <div className='flex flex-col md:flex-row md:items-center gap-3 mt-2'>
+                <div className='flex flex-col md:flex-row md:items-center gap-3 w-full'>
+                    <Button href="https://www.tiktok.com/@elomadecrochet" target="_blank" icon="tiktok" variante="stroke-red" className="basis-full">elomadecrochet</Button>
+                    <Button href="https://www.instagram.com/elomadecrochet" target="_blank" icon="instagram" variante="stroke-red" className="basis-full">elomadecrochet</Button>
                 </div>
                 <Button icon="email" variante="fill-red" href="mailto:contact@elomade.fr" className="basis-full" target="_blank">contact@elomade.fr</Button>
             </div>
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -7,13 +7,13 @@ const Heading = ({ children, className, level, tag, ...props }) => {
 
     switch (level) {
         case '1':
-            style += 'text-[32px] lg:text-[52px] font-semibold leading-[130%]'
+            style += 'text-[28px] lg:text-[40px] font-semibold leading-[130%]'
             break
         case '2':
             style += 'text-[32px] lg:text-[40px] font-semibold leading-[130%] '
             break
         case '3':
-            style += 'text-[20px] font-medium leading-[160%] '
+            style += 'text-[18px] lg:text-[20px] font-medium leading-[160%] '
             break
         case '4':
             style += 'text-[16px] font-medium leading-[160%] '
