bad30e7ae087b9cf0883aef5fc229d8005c9f52b
Tom CHARLET <charlettom11@gmail.com>
2025-06-04 23:22:41 +0200

    Mise à jour responsive slider

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,14 +17,14 @@ const Product = ({ title, categories, images, price, minPrice, maxPrice, text, a
     return <section className='responsive-container w-full mt-16 lg:mt-[112px] lg:min-h-[calc(100svh-112px)] py-16 lg:pb-16 lg:pt-12 flex flex-col lg:flex-row gap-10 lg:gap-20'>
         <motion.div variants={isMobile ? fadeUp : fadeRight} initial="hidden" whileInView="visible" viewport={{ once: true }} className='w-full lg:w-[60%] shrink-0 h-full'>
             {images?.length > 1 && <Slider
-                className="!aspect-[11/14] md:!aspect-[16/12] xl:!aspect-[16/10] lg:!w-full !rounded-3xl"
+                className="!aspect-[11/14] md:!aspect-[16/12] xl:!aspect-[16/10] lg:!w-full !rounded-3xl !max-h-[70svh] md:max-h-auto !w-full md:!w-auto"
                 spaceBetween={16}
                 pagination
                 freeMode={true}
                 mousewheel={true}
             >
                 {images.map((item, index) => <span key={index} className='w-auto h-auto bg-beige-300'>
-                    <Image key={index} src={item.url ?? "/placeholder.svg"} alt={item.alt ?? title} width={0} height={0} sizes='100vw' className='rounded-3xl w-auto h-full object-cover min-w-[80svw] md:min-w-auto' />
+                    <Image key={index} src={item.url ?? "/placeholder.svg"} alt={item.alt ?? title} width={0} height={0} sizes='100vw' className={`rounded-3xl w-auto h-full object-cover min-w-[80svw] md:min-w-[50svw] ${images?.length == 2 ? "lg:min-w-[calc(25svw-32px)]" : "lg:min-w-auto"}`} />
                 </span>)}
             </Slider>}
             {(!images || images?.length == 0 || images?.length == 1) && <Image src={images?.length > 0 ? images[0]?.url : "/placeholder.svg"} alt={images?.length > 0 ? (images[0]?.alt ?? title) : "Aucune photo"} width={0} height={0} sizes='100vw' className={`rounded-3xl w-full ${!images || images?.length == 0 ? "aspect-[9/10]" : "h-auto"} h-auto md:aspect-[16/12] xl:aspect-[16/10] object-cover max-h-[70svh]`} />}
