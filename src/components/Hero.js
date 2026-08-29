6047aa7d7c1c205a38afb3ba861ae46257479e11
Tom CHARLET <charlettom11@gmail.com>
2026-08-29 17:50:31 +0200

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -27,7 +27,7 @@ const Hero = () => {
                     <Button variants={fadeUp} icon="chevron-right" reverse className="md:self-start" href="#les-creations" variante="stroke-beige">Découvrir l&apos;univers</Button>
                 </motion.div>
             </motion.div>
-            <div className='absolute inset-0 grid items-center justify-center after:content-[""] after:absolute after:inset-0 after:z-10 after:lg:hidden after:bg-[linear-gradient(180deg,rgba(0,0,0,0)_40%,rgba(0,0,0,0.4)_100%)] h-full w-full'>
+            <div className='absolute bg-beige-300 grid items-center justify-center after:content-[""] after:absolute after:inset-0 after:z-10 after:lg:hidden after:bg-[linear-gradient(180deg,rgba(0,0,0,0)_40%,rgba(0,0,0,0.4)_100%)] h-full w-full'>
                 <LazyMotion features={domAnimation}>
                     <m.div
                         style={{ y: y ?? null }}
